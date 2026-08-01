8fbd01dcd570f528275821523bed8cc054aa350d
Ryan <ryanczpereira@gmail.com>
2026-08-01 13:10:01 -0300

    removing viper

--- internal/infra/database/turso/connection.go
+++ internal/infra/database/turso/connection.go
@@ -3,18 +3,10 @@ package turso
 import (
 	"database/sql"
 
-	"github.com/rcovery/go-stock-control/internal/config"
 	_ "turso.tech/database/tursogo"
 )
 
-func GetConnectionFromEnv() string {
-	path := config.GetString("TURSO_DATABASE_PATH")
-	if path == "" {
-		path = "app.db"
-	}
-
-	return path
-}
+const DatabasePath = "app.db"
 
 func NewDatabaseConnection(connectionString string) (*sql.DB, error) {
 	db, err := sql.Open("turso", connectionString)
