5fa6adeaea76e7870de7873fa4a8f434ea6cfd9b
Ryan <ryanczpereira@gmail.com>
2026-08-01 01:32:53 -0300

    initializing http handlers

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/rcovery/go-stock-control/internal/config"
 	"github.com/rcovery/go-stock-control/internal/http/handlers"
-	database_postgres "github.com/rcovery/go-stock-control/internal/infra/database/postgres"
+	parts_database "github.com/rcovery/go-stock-control/internal/infra/database/postgres"
 	"github.com/rcovery/go-stock-control/internal/part"
 	"github.com/rcovery/go-stock-control/internal/part/postgres"
 )
@@ -16,15 +16,16 @@ import (
 func main() {
 	config.InitConfig()
 
-	connectionString := database_postgres.GetConnectionFromEnv()
-	db, databaseErr := database_postgres.NewDatabaseConnection(connectionString)
+	connectionString := parts_database.GetConnectionFromEnv()
+	db, databaseErr := parts_database.NewDatabaseConnection(connectionString)
 	if databaseErr != nil {
 		panic(databaseErr)
 	}
 
 	repoInstance := postgres.NewRepository(db)
 	serviceInstance := part.NewService(repoInstance)
-	handlers.HandlePart(serviceInstance)
+	partHandler := handlers.NewPartHandler(serviceInstance)
+	partHandler.HandlePart()
 
 	host := config.GetString("HOST")
 	port := config.GetString("PORT")
