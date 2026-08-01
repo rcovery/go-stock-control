f6fa9235e4f4d52a15c275ae2cd90a3d32e5253b
Ryan <ryanczpereira@gmail.com>
2026-07-31 23:20:41 -0300

    base service parts

--- internal/part/service.go
+++ internal/part/service.go
@@ -1,1 +1,44 @@
 package part
+
+import "context"
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) Create(ctx context.Context, p Part) (Part, error) {
+	newID, err := NewID()
+	if err != nil {
+		return Part{}, err
+	}
+	p.ID = newID
+
+	err = s.repo.Create(ctx, p)
+	if err != nil {
+		return Part{}, err
+	}
+
+	return p, nil
+}
+
+func (s *Service) List(ctx context.Context) ([]Part, error) {
+	return s.repo.List(ctx)
+}
+
+func (s *Service) ListByCategory(ctx context.Context, category string) ([]Part, error) {
+	return s.repo.ListByCategory(ctx, category)
+}
+
+func (s *Service) Update(ctx context.Context, p Part) error {
+	return s.repo.Update(ctx, p)
+}
+
+func (s *Service) Delete(ctx context.Context, id ID) error {
+	return s.repo.Delete(ctx, id)
+}
